0656be20b2b7bd267f77f26494612e92fe01d050
DaWanAnOnli <joel.rompis@gmail.com>
2024-04-06 21:05:24 +0700

    initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/bepayment/BePaymentApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BePaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BePaymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/bepayment/BePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bepayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BePaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
