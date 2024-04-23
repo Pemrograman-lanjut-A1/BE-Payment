6e4b4f54debdcdc8ba711cf461a53dad28445a72
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2024-04-23 16:13:21 +0700

    [REFACTOR] Apply TopUpStatus enum check to TopUp model.

--- src/main/java/id/ac/ui/cs/advprog/bepayment/model/TopUp.java
+++ src/main/java/id/ac/ui/cs/advprog/bepayment/model/TopUp.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.bepayment.model;
 
+import id.ac.ui.cs.advprog.bepayment.enums.TopUpStatus;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,19 +9,22 @@ import java.util.Arrays;
 
 @Builder
 @Getter
+@Setter
 public class TopUp {
     String id;
     String userId;
     Wallet wallet;
     double amount;
-    String status;
+    TopUpStatus status;
 
-    public void setStatus(String status){
-        String[] statusList = {"WAITING_APPROVAL", "REJECTED", "SUCCESS", "CANCELED"};
-        if(Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))){
+    public void setStatus(TopUpStatus status) {
+        this.status = status;
+    }
+    public void setAmount(double amount){
+        if(amount <= 0){
             throw new IllegalArgumentException();
         }else{
-            this.status = status;
+            this.amount = amount;
         }
     }
 }
