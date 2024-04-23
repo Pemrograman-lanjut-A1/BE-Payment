e9c55eb5c1b72938d199205e2c8b75d1a325eca6
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2024-04-23 14:26:24 +0700

    [REFACTOR] Implement Builder Pattern for Top Up Model

--- src/main/java/id/ac/ui/cs/advprog/bepayment/model/TopUp.java
+++ src/main/java/id/ac/ui/cs/advprog/bepayment/model/TopUp.java
@@ -13,38 +13,7 @@ public class TopUp {
     String userId;
     Wallet wallet;
     double amount;
-    @Setter
     String status;
-
-    public TopUp(String id, String userId, Wallet wallet, double amount) {
-        this.id = id;
-        this.userId = userId;
-        this.wallet = wallet;
-        this.amount = amount;
-        this.status = "WAITING_APPROVAL";
-
-        if (wallet == null) {
-            throw new IllegalArgumentException("Wallet must not be null");
-        }
-
-        if (userId == null) {
-            throw new IllegalArgumentException("User not found");
-        }
-
-    }
-    public TopUp(String id, String userId, Wallet wallet, double amount, String status) {
-        this(id,userId,wallet,amount);
-
-        String[] statusList = {"WAITING_APPROVAL", "REJECTED", "SUCCESS", "CANCELED"};
-        if(Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))){
-            throw new IllegalArgumentException();
-        }else{
-            this.status = status;
-        }
-
-
-    }
-
 
     public void setStatus(String status){
         String[] statusList = {"WAITING_APPROVAL", "REJECTED", "SUCCESS", "CANCELED"};
