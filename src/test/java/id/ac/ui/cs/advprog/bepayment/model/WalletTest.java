7ceb5e50b9390b920139a8cd7b2e4c4d01e17c4b
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2024-04-24 19:31:28 +0700

    [RED] Add tests for Wallet model.

---
+++ src/test/java/id/ac/ui/cs/advprog/bepayment/model/WalletTest.java
@@ -0,0 +1,39 @@
+package id.ac.ui.cs.advprog.bepayment.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class WalletTest {
+    private Wallet wallet;
+
+    @BeforeEach
+    void setUp() {
+        wallet = new Wallet(
+                "1",
+                "3df9d41b-33c3-42a1-b0a4-43cf0ffdc649",
+                500);
+    }
+
+    @Test
+    void testCreateWallet() {
+        assertNotNull(wallet);
+        assertEquals("1", wallet.getId());
+        assertEquals("3df9d41b-33c3-42a1-b0a4-43cf0ffdc649", wallet.getUserId());
+        assertEquals(500, wallet.getAmount());
+    }
+
+    @Test
+    void testSetUserId() {
+        wallet.setUserId("user456");
+        assertEquals("user456", wallet.getUserId());
+    }
+
+    @Test
+    void testSetAmount() {
+        wallet.setAmount(1000);
+        assertEquals(1000, wallet.getAmount());
+    }
+}
