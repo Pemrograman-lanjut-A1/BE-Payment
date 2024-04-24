9e609767a5e4678ed5206d2737fb8ac0a83b0773
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2024-04-24 19:34:22 +0700

    [RED] Add Wallet model skeleton.

--- src/main/java/id/ac/ui/cs/advprog/bepayment/model/Wallet.java
+++ src/main/java/id/ac/ui/cs/advprog/bepayment/model/Wallet.java
@@ -1,14 +1,25 @@
 package id.ac.ui.cs.advprog.bepayment.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Builder
+@Entity
 @Getter
+@Setter
+@NoArgsConstructor
 public class Wallet {
-    String id;
-    String userId;
-    @Setter
-    Integer amount;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String userId;
+    private Integer amount;
+
+    public Wallet(String id, String userId, int amount) {
+    }
 }
