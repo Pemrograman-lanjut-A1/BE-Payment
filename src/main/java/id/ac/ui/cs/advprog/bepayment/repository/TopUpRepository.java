3966471ce1bbd0e53aa07bf1077f290c593dabdd
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2024-04-27 18:12:04 +0700

    [RED] Add TopUpRepository Skeleton.

--- src/main/java/id/ac/ui/cs/advprog/bepayment/repository/TopUpRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/bepayment/repository/TopUpRepository.java
@@ -1,4 +1,14 @@
 package id.ac.ui.cs.advprog.bepayment.repository;
 
-public class TopUpRepository {
+import id.ac.ui.cs.advprog.bepayment.model.TopUp;
+
+import java.util.List;
+
+public interface TopUpRepository {
+    public TopUp save(TopUp topUp);
+    public void deleteAll();
+    public void deleteTopUpById(String topUpId);
+    public void cancelTopUp(String topUpId);
+    public TopUp findById(String id);
+    public List<TopUp> findAll();
 }
