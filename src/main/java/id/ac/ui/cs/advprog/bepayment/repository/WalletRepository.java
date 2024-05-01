6d05b09db87d651a3570ebf0d8a1a6fee5afb6ab
Faris Zhafir Faza <111985990+Marsupilamieue@users.noreply.github.com>
2024-05-01 15:00:55 +0700

    [GREEN] Implement findByUserId in WalletRepository.

--- src/main/java/id/ac/ui/cs/advprog/bepayment/repository/WalletRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/bepayment/repository/WalletRepository.java
@@ -7,5 +7,6 @@ import id.ac.ui.cs.advprog.bepayment.model.Wallet;
 public interface WalletRepository {
     public Wallet save(Wallet wallet);
     public Wallet findById(String id);
+    public Wallet findByUserId(String userId);
     public void addAmount(String id, double totalAmount);
 }
